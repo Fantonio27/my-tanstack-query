3148fbe1ac49c4d7cfb8ca3417d392ce4f2ee138
Francis <francis.antonio0927@gmail.com>
2025-04-20 20:24:11 +0800

    Implement useUser hook with React Query for fetching users

--- src/hooks/query/users.ts
+++ src/hooks/query/users.ts
@@ -1,1 +1,10 @@
-   
+import { useQuery } from "@tanstack/react-query";
+import { userService } from "../../data/users";
+ 
+export const useUser = () => {
+    return useQuery({
+      queryKey: ["users"],
+      queryFn: userService.getUsers,
+      staleTime: 1000,
+    });
+}
